3a91f8f1849469f1b2fc4a5775f9c41e2da13ad7
FALAK92 <naaz92falak@gmail.com>
2024-11-01 19:27:39 +0500

    multipage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import localFont from "next/font/local";
+import Header from "./component/header";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><Header/>
         {children}
       </body>
     </html>
